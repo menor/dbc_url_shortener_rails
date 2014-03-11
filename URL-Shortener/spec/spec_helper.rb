8e1b44e0886e7a42b68230025aa8053c44db2ac5
José Menor & Ivan Sued <pair+menor.thefenry@devbootcamp.com>
2014-03-11 16:34:48 -0700

    Change the cleaner helper to delete everything after every test.

--- URL-Shortener/spec/spec_helper.rb
+++ URL-Shortener/spec/spec_helper.rb
@@ -9,7 +9,8 @@ RSpec.configure do |config|
  config.use_transactional_fixtures = false
 
     config.before(:suite) do
-      DatabaseCleaner.strategy = :truncation
+      DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
     end
 
     # config.before(:each, js: true) do
